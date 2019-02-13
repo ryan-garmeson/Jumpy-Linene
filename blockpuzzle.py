9bb95a6428ad054fe1327b8a847680fd2aada58e
Ryan Garmeson <ryan.garmeson@gmail.com>
2019-02-12 18:00:26 -0600

    Update blockpuzzle.py
    Added rotate in place function

--- blockpuzzle.py
+++ blockpuzzle.py
@@ -19,6 +19,43 @@ class Piece:
     self.color = color
     self.unit_list = unit_list
     self.unit_count = len(unit_list)
+  
+  def zeroup(self):
+    xmin = min(x[0] for x in self.unit_list)
+    ymin = min(x[1] for x in self.unit_list)
+    if min([xmin, ymin, 0]) != 0:      
+      if xmin < ymin:
+        self.unit_list = [[t[0] - xmin, t[1]] for t in self.unit_list]
+      else:
+        self.unit_list = [[t[0], t[1] - ymin] for t in self.unit_list]
+
+  #rotate 90 degrees counterclockwise
+  def rotate90(self):
+    for i, item in enumerate(self.unit_list):
+      x = item[0]
+      y = item[1]
+      self.unit_list[i][0] = -1 * y
+      self.unit_list[i][1] = x
+    self.zeroup()
+  
+  #rotate 90 degrees clockwise
+  def antirotate90(self):
+    for i, item in enumerate(self.unit_list):
+      x = item[0]
+      y = item[1]
+      self.unit_list[i][0] = y
+      self.unit_list[i][1] = -1 * x
+    self.zeroup()
+
+  def rotate(self, turns):
+    if turns > 0:
+      for i in range(turns):
+        self.rotate90()
+    else:
+      turns = turns * -1
+      for i in range(turns):
+        self.antirotate90()
+    return self
 
 hole = Piece('black',[[0,0]])
 pieces = [
@@ -39,9 +76,13 @@ def willFit(board, piece, location):
       return False
   return True
 
+def validLocation(x,y):
+  return x >= 0 and x < 7 and y >=0 and y < 7
+
 def placePiece(board, piece, location):
-  if willFit(board, piece, location):
-    for unit in piece.unit_list:
+  #if willFit(board, piece, location):
+  for unit in piece.unit_list:
+    if validLocation(location[0]+unit[0],location[1]+unit[1]):
       board[location[0]+unit[0]][location[1]+unit[1]] = (piece.unit_count, piece.color)
   return board
 
@@ -55,6 +96,41 @@ def printBoard(A):
         print('%s%s %s' % (fg('white'), bg('white'), attr('reset')), end='')
     print(' ')
 #how to use it
-A = placePiece(A, pieces[0], [0,0])
+#hole
+A = placePiece(A, hole, H[11])
+#red 3x3
+A = placePiece(A, pieces[0].rotate(-4), [1,0])
+printBoard(A)
+print(' ')
+#yellow 3x3
+A = placePiece(A, pieces[1], [1,3])
+printBoard(A)
+print(' ')
+#green 3x4
+A = placePiece(A, pieces[2].rotate(1), [3,0])
+printBoard(A)
+print(' ')
+#teal 1x5
+A = placePiece(A, pieces[3].rotate(1), [6,2])
+printBoard(A)
+print(' ')
+#orange 1x2
+A = placePiece(A, pieces[4].rotate(1), [5,4])
+printBoard(A)
+print(' ')
+#red 2x4 
+A = placePiece(A, pieces[5].rotate(2), [2,3])
+printBoard(A)
+print(' ')
+#purple 3x3
+A = placePiece(A, pieces[6].rotate(2), [0,4])
+printBoard(A)
+print(' ')
+#purple 2x3 
+A = placePiece(A, pieces[7].rotate(1), [3,5])
+printBoard(A)
+print(' ')
+#blue 2x4
+A = placePiece(A, pieces[8], [0,0])
 #print to terminal
 printBoard(A)
